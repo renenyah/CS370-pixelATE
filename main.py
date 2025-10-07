126e20a9f7ba6cb83f602ce51b6f035cf9bac579
renenyah <nyahshops@gmail.com>
2025-10-06 23:54:30 -0400

    deleted old file

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-print("Nyah Rene is taking CS370")
-print("Hi there!")
+import pdfplumber
+
+with pdfplumber.open("syllabus.pdf") as pdf:
+    page = pdf.pages[0]
+    text = page.extract_text()
+    print(text)
+
+    

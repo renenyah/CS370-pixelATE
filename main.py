191642b11ec5af67f331c35a74e70cb75727397a
oliviajoym <olivia.joyner2021@gmail.com>
2025-10-08 00:52:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from PyPDF2 import PdfReader
 #gets file
 reader = PdfReader("mathSyllabus.pdf")
 
+#extracts text by page
 with open("extracted.txt", "w", encoding="utf-8") as f:
 
     for page_num, page in enumerate(reader.pages):
         txt = page.extract_text()
+        
+        #first writes page number
         f.write(f"-----Page {page_num + 1}----- \n")
         if txt:
             f.write(txt + "\n")

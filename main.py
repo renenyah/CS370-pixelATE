9a2a486dd88086dcd120c7847c5ccf1b540afa23
oliviajoym <olivia.joyner2021@gmail.com>
2025-10-08 01:12:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ with open("extracted.txt", "w", encoding="utf-8") as f:
 
     for page_num, page in enumerate(reader.pages):
         txt = page.extract_text()
-        
+
         #first writes page number
         f.write(f"-----Page {page_num + 1}----- \n")
         if txt:
             f.write(txt + "\n")
-        f.write("\n\n")
+        f.write("\n\n") # space after each page

8c692092b09f81605933f670796d81166e62bcaf
oliviajoym <olivia.joyner2021@gmail.com>
2025-10-08 00:35:44 -0400

    Test??
    I don't know if this is going to work

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+from PyPDF2 import PdfReader
+
+reader = PdfReader("mathSyllabus.pdf")
+
+with open("extracted.txt", "w") as f:
+
+    for page in reader.pages:
+        txt = page.extract_text()
+        if t:
+            f.write(t + "\n")

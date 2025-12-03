a0429c76efb94af98fc343ebc1969c70e671fda1
renenyah <nyahshops@gmail.com>
2025-12-03 17:41:56 -0500

    Revert "user authentication available to anyone"
    This reverts commit 4c0cf357679354a3a59e3b9291696a30fb21992f.

--- pixelate/constant/config.ts
+++ pixelate/constant/config.ts
@@ -1,6 +1,3 @@
 // app/config.ts
 export const API_BASE_URL =
   process.env.EXPO_PUBLIC_API_BASE_URL ?? "http://localhost:8000";
-
-export const REDIRECT_URL =
-  process.env.EXPO_PUBLIC_REDIRECT_URL ?? "exp://localhost:8081/";
